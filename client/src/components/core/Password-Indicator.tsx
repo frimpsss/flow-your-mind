df3f8c2f6568e911a79c071f379e041bce04226a
frimpsss <afrimpong031@gmail.com>
2023-12-12 11:09:43 +0000

    fix for add key

--- client/src/components/core/Password-Indicator.tsx
+++ client/src/components/core/Password-Indicator.tsx
@@ -37,9 +37,9 @@ const PasswordIndicator = ({
   ];
   return (
     <div className="mt-2">
-      {tests.map((e) => {
+      {tests.map((e, inx) => {
         return (
-          <div className={`${visible ? "block" : "none"}`}>
+          <div className={`${visible ? "block" : "none"}`} key={inx}>
             <PasswordStrenghtTile
               text={e.title}
               passed={e.passed}
