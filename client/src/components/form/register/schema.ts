61fdb0de8f7d4ad50f58b7d6f6b259ffff75ab9a
frimpsss <afrimpong031@gmail.com>
2023-12-23 21:52:32 +0000

    fix password strengh

--- client/src/components/form/register/schema.ts
+++ client/src/components/form/register/schema.ts
@@ -2,9 +2,12 @@ import * as Vagina from "yup";
 
 export const RegisterSchema = Vagina.object().shape({
   username: Vagina.string().required("Username is required"),
-  password: Vagina.string().matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/,
-    { message: "Weak password" }
-  ).required('password is required'),
-  confirmPassword: Vagina.string().oneOf([Vagina.ref('password')], 'Passwords dont match').required('password is required')
+  password: Vagina.string()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/, {
+      message: "Weak password",
+    })
+    .required("password is required"),
+  confirmPassword: Vagina.string()
+    .oneOf([Vagina.ref("password")], "Passwords dont match")
+    .required("password is required"),
 });
