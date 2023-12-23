cf5d9f61805a315cdec0feb692c86a53f8acc3e6
frimpsss <afrimpong031@gmail.com>
2023-12-23 21:14:00 +0000

    update cors andd live url

--- client/src/app/[username]/page.tsx
+++ client/src/app/[username]/page.tsx
@@ -10,9 +10,9 @@ async function getUserId(username: string) {
       return response?.data?.data;
     }
   } catch (error: unknown) {
-    console.log("error:  "+error);
-    
-    throw error;
+    console.log("error:  " + error);
+
+    throw new Error("link is broken");
   }
 }
 const Page = async ({ params }: { params: { username: string } }) => {
--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,7 +1,10 @@
 export { default as CustomResponse } from "./wrapper";
 export { HttpStatusCode } from "./globalTypes";
 import { CorsOptions } from "cors";
-const whiteList = ["http://localhost:3000", "https://google.com"];
+const whiteList = [
+  "http://localhost:3000",
+  "https://flow-your-mind-test.vercel.app",
+];
 export const corsOptions: CorsOptions = {
   origin(requestOrigin, callback) {
     if (whiteList.includes(requestOrigin as string) || !requestOrigin) {
